3487e33e70c648fb9ac5afc8a08e35e88430c9a0
deetoo <doug.dobies@gmail.com>
2018-10-30 14:12:13 -0500

    commented loginfailures.sh

--- loginfailures.sh
+++ loginfailures.sh
@@ -2,7 +2,9 @@
 MONTHNAME=`date +%b`
 DAY=`date +%d`
 
+# parse the number of failed logins for the current day.
 FAILS=`cat /var/log/secure | grep "Failed password" | grep "$MONTHNAME $DAY" -c`
 
+# push current number of failed logins, on the current day, into the DB
 mysql -u DB_USER -pDB_PASSWORD DB_NAME -e "INSERT INTO TABLE_NAME VALUES('0', NOW(), '$FAILS' );"
 
